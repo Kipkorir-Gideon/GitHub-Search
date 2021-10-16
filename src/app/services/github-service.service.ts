697f515c5b71cef219075086cac03011511b25c6
Kipkorir-Gideon <gideon.ngetich@student.moringaschool.com>
2021-10-16 23:27:04 +0300

    Add a function to find user's repositories(public).

--- src/app/services/github-service.service.ts
+++ src/app/services/github-service.service.ts
@@ -37,5 +37,29 @@ export class GithubServiceService {
     return promise;
   }
 
-  findRepositories()
+  findRepositories(usersRepo: string) {
+    interface repoInterface {
+      name:String,
+      description:string,
+      timeCreated:Date
+    }
+
+    let urlRepo = 'https://api.github.com/users/' + usersRepo + '/repos';
+
+    let promise = new Promise((resolve, reject) => {
+      this.http.get<repoInterface[]>(urlRepo).toPromise().then((results) => {
+        this.repositories = [];
+        for (let i = 0; i < results.length; i++) {
+          let repo = new Repositories(results[i].name, results[i].description, results[i].timeCreated);
+          this.repositories.push(repo);
+        }
+        resolve(results);
+      },
+      (error) => {
+        reject();
+      });
+    });
+    return promise;
+
+  }
 }
